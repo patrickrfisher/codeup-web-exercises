ccdea4282a4e19dbf9ae711b1d77328201600552
mac <mac@macs-macbook-pro.local>
2018-08-14 11:44:39 -0500

    8/14/2018 starting arrays exercises

---
+++ js/practicejs.js
@@ -0,0 +1,14 @@
+var writeOut = function (input) {
+    for (var i=0;i<input.length; i++){
+        console.log(input[i]);
+    }
+}
+var fruits = ["apple", "avocado", "pomegranate", "pineapple", "jackfruit"];
+
+writeOut(fruits);
+// var shapes = ['square', 'rectangle', 'circle', 'triangle', 'prism'];
+//
+// // loop through the array and log the values
+// for (var i = 0; i < shapes.length; i++) {
+//     console.log('The shape at index ' + i + ' is: ' + shapes[i]);
+// }
