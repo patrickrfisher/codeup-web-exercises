d029e0dc912fe08d8b8ebf1421a5cb45fe440a6a
mac <mac@macs-macbook-pro.local>
2018-08-15 11:40:00 -0500

    8/15 arrays

--- js/practicejs.js
+++ js/practicejs.js
@@ -1,14 +1,6 @@
-var writeOut = function (input) {
-    for (var i=0;i<input.length; i++){
-        console.log(input[i]);
-    }
-}
-var fruits = ["apple", "avocado", "pomegranate", "pineapple", "jackfruit"];
-
-writeOut(fruits);
-// var shapes = ['square', 'rectangle', 'circle', 'triangle', 'prism'];
-//
-// // loop through the array and log the values
-// for (var i = 0; i < shapes.length; i++) {
-//     console.log('The shape at index ' + i + ' is: ' + shapes[i]);
-// }
+var bugs = ["roach", "dragonfly","spider","mosquito",
+            "ladybug","earthworm","wasp", "centipede",
+            "ant", "tick", "earwig",
+            "silverfish","aphid","junebug","millipede"
+            ,"beetle","cricket","grasshopper"];
+//this is a array function, you can tell arrays by using brackets [--]
