dcc3af2308a3f218dd7a68322972368b781fcd45
mac <mac@macs-macbook-pro.local>
2018-08-16 08:23:50 -0500

    8/15 arrays

--- js/practicejs.js
+++ js/practicejs.js
@@ -1,6 +1,8 @@
-var bugs = ["roach", "dragonfly","spider","mosquito",
-            "ladybug","earthworm","wasp", "centipede",
-            "ant", "tick", "earwig",
-            "silverfish","aphid","junebug","millipede"
-            ,"beetle","cricket","grasshopper"];
-//this is a array function, you can tell arrays by using brackets [--]
+function allIndexesOf={
+
+}
+var fruits = ["apple", "banana", "orange", "apple", "pineapple"];
+allIndexesOf(fruits, "apple")
+allIndexesOf(fruits, "guava")
+allIndexesOf(fruits, "pineapple")
+k
