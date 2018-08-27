1ed9a1011cb56fbf07c25f2ec769b45591b47ec0
mac <mac@macs-macbook-pro.local>
2018-08-27 16:42:23 -0500

    8/21 finished the buttons for eventlisteners

--- js/jqueryplay.js
+++ js/jqueryplay.js
@@ -1,1 +1,28 @@
-$(function ())
+$(function () {
+    $("#btn1").click(function() {
+        $("#panel1").slideToggle(1000).delay.slideToggle(1000);
+        $("#panel2").slideUp(1000);
+        $("#panel3").slideUp(1000);
+        $("#panel4").slideUp(1000);
+
+    });
+        $("#btn2").dblclick(function (){
+            $("#panel2").fadeToggle(1000);
+
+    });
+        $("#btn3").mouseover(function() {
+            $("#panel3"). toggle(1000);
+        });
+        $("#btn4").mouseover(function(){
+            $("#panel4"). toggle(1000);
+        })
+
+
+    slideUp, slideDown, slideToggle
+    show, hide, toggle
+    delay
+    fadeIn,fadeOut,fadeToggle
+    addClass, removeClass, toggleClass
+    on, click, dblclick, mouseover, hover
+});
+//todo jquery mouse exercise
